b9bcba4d6ae0bb13265892ecaf83f2b760b18221
PremPPrem <onusoka.thaika@gmail.com>
2023-08-22 04:46:00 +0700

    add css in modal component

--- pages/components/Modal.jsx
+++ pages/components/Modal.jsx
@@ -4,17 +4,17 @@ import s from "@/styles/Modal.module.scss";
 
 export default function Modal({id,deleteTodo,closeModal}) {
   return (
-    <div>
-    <div>
-      <div>
-          <button onClick={()=> closeModal(false)}>x</button>
-          <div><BiErrorCircle /></div>
-          <div>
+    <div className={s.modal}>
+    <div className={s.modal_header}>
+      <div className={s.modal_content}>
+          <button onClick={()=> closeModal(false)} className={s.modal_btn_x}>x</button>
+          <div className={s.modal_icon}><BiErrorCircle /></div>
+          <div className={s.modal_title}>
               <p>Do you want to delete</p>
           </div>
-          <div>
-              <button onClick={()=>deleteTodo(id)}>Yes</button>
-              <button onClick={()=> closeModal(false)}>No</button>
+          <div className={s.modal_button}>
+              <button onClick={()=>deleteTodo(id)} className={`${s.modal_btn} ${s.modal_btn_yes}`}>Yes</button>
+              <button onClick={()=> closeModal(false)} className={`${s.modal_btn} ${s.modal_btn_no}`}>No</button>
           </div>
       </div>
     </div>
